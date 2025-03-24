f8e6869634efa237380044d1a007c4754d710af7
ccastano46 <castanocamilo522@gmail.com>
2025-03-23 19:17:07 -0500

    Sin docker

--- src/main/java/arsw/tamaltolimense/playermanager/PlayerManager.java
+++ src/main/java/arsw/tamaltolimense/playermanager/PlayerManager.java
@@ -1,14 +1,9 @@
 package arsw.tamaltolimense.playermanager;
 
-import com.mongodb.*;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoDatabase;
-import org.bson.Document;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.github.cdimascio.dotenv.Dotenv;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
 
 @SpringBootApplication
 public class PlayerManager {
