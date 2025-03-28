d71e9f042b4b1942e2cc331b49f9ef0c01c5a5a3
ccastano46 <castanocamilo522@gmail.com>
2025-03-28 06:49:18 -0500

    Without SokectJS

--- src/main/java/arsw/tamaltolimense/playermanager/config/WebSocketConfig.java
+++ src/main/java/arsw/tamaltolimense/playermanager/config/WebSocketConfig.java
@@ -19,6 +19,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/balance").setAllowedOrigins("*");
+        registry.addEndpoint("/balance").setAllowedOrigins("*").withSockJS();
     }
 }
