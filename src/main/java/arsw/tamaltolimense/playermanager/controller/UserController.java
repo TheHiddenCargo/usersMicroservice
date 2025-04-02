83f700bab0315783fd2c19c59945f3ba29d55b5e
ccastano46 <castanocamilo522@gmail.com>
2025-04-02 06:50:52 -0500

    Quitando Ciertos API

--- src/main/java/arsw/tamaltolimense/playermanager/controller/UserController.java
+++ src/main/java/arsw/tamaltolimense/playermanager/controller/UserController.java
@@ -49,31 +49,6 @@ public class UserController {
         }
     }
 
-    @PutMapping("/update/nickname")
-    public ResponseEntity<Object> nickName(@RequestBody Map<String, String> userData) {
-        try{
-            String email = userData.get(EMAIL);
-            String newNickName = userData.get("newNickname");
-            userService.updateNickName(email,newNickName);
-            return new ResponseEntity<>(userService.getUserInfo(email),HttpStatus.OK);
-        }catch (UserException e){
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
-    }
-
-    @PutMapping("/update/photo")
-    public ResponseEntity<Object> photo(@RequestBody Map<String, String> userData) {
-        try{
-            String email = userData.get(EMAIL);
-            String newPhoto = userData.get("newPhoto");
-            userService.updateNickName(email,newPhoto);
-            userService.updatePhoto(email,newPhoto);
-            return new ResponseEntity<>(userService.getUserInfo(email),HttpStatus.OK);
-        }catch (UserException e){
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
-    }
-
 
     @DeleteMapping("/delete/{email}")
     public ResponseEntity<Object> deleteUser(@PathVariable(EMAIL) String email) {
