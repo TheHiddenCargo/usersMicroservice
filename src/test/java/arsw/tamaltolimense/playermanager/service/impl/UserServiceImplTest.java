5c28946d4950f464f592a7c6e454d4d667c55e82
ccastano46 <castanocamilo522@gmail.com>
2025-05-13 22:02:17 -0500

    Pruebas

--- src/test/java/arsw/tamaltolimense/playermanager/service/impl/UserServiceImplTest.java
+++ src/test/java/arsw/tamaltolimense/playermanager/service/impl/UserServiceImplTest.java
@@ -101,7 +101,7 @@ class UserServiceImplTest {
     @Test
     void shouldNotRegisterNullNickName(){
         try{
-           userService.registerUser("casbsuw@mail.com",null,5000,"imagen");
+            userService.registerUser("casbsuw@mail.com",null,5000,"imagen");
             fail("Should have thrown exception");
         }catch (UserException e){
             assertEquals(UserException.NULL_VALUE,e.getMessage());
@@ -138,7 +138,7 @@ class UserServiceImplTest {
             assertEquals(UserException.NULL_VALUE,e.getMessage());
         }
     }
-    
+
     @Test
     void shouldNotRegisterNegativeBalance(){
         try{
@@ -258,8 +258,8 @@ class UserServiceImplTest {
         }
     }
 
-   @Test
-   void shouldDoTransaction(){
+    @Test
+    void shouldDoTransaction(){
         try{
             User user = new User("casbsuw@mail.com", "milo45",500,"image");
             when(userRepository.findByEmail(user.getEmail())).thenReturn(user);
@@ -275,109 +275,159 @@ class UserServiceImplTest {
         }catch (UserException e){
             fail(e.getMessage());
         }
-   }
+    }
 
-   @Test
+    @Test
     void shouldUpdateConcurrent() {
-       User user = new User("casbsuw@mail.com", "milo45",500,"image");
-       when(userRepository.findByEmail(user.getEmail())).thenReturn(user);
+        User user = new User("casbsuw@mail.com", "milo45",500,"image");
+        when(userRepository.findByEmail(user.getEmail())).thenReturn(user);
 
-       Thread thread1 = new Thread(() -> {
-           try {
-               userService.updateNickName("casbsuw@mail.com", "ccastano42");
-               userService.transaction("casbsuw@mail.com", -100);
-           } catch (UserException e){
-               fail(e.getMessage());
-           }
-       });
+        Thread thread1 = new Thread(() -> {
+            try {
+                userService.updateNickName("casbsuw@mail.com", "ccastano42");
+                userService.transaction("casbsuw@mail.com", -100);
+            } catch (UserException e){
+                fail(e.getMessage());
+            }
+        });
 
-       Thread thread2 = new Thread(() -> {
-           try {
+        Thread thread2 = new Thread(() -> {
+            try {
 
-               userService.transaction("casbsuw@mail.com", 1000);
-           }catch (UserException e){
-               fail(e.getMessage());
-           }
-       });
+                userService.transaction("casbsuw@mail.com", 1000);
+            }catch (UserException e){
+                fail(e.getMessage());
+            }
+        });
 
-       Thread thread3 = new Thread(() -> {
-           try {
-               userService.transaction("casbil.com", 1000);
-           }catch (UserException e){
-               System.out.println("casbil.com "+e.getMessage());
-           }
-       });
+        Thread thread3 = new Thread(() -> {
+            try {
+                userService.transaction("casbil.com", 1000);
+            }catch (UserException e){
+                System.out.println("casbil.com "+e.getMessage());
+            }
+        });
 
-       thread1.start();
-       thread2.start();
-       thread3.start();
+        thread1.start();
+        thread2.start();
+        thread3.start();
 
 
-       try{
-           thread1.join();
-           thread2.join();
-           thread3.join();
+        try{
+            thread1.join();
+            thread2.join();
+            thread3.join();
 
-           ArgumentCaptor<User> userCaptor = ArgumentCaptor.forClass(User.class);
+            ArgumentCaptor<User> userCaptor = ArgumentCaptor.forClass(User.class);
 
-           verify(userRepository, times(3)).save(userCaptor.capture());
+            verify(userRepository, times(3)).save(userCaptor.capture());
 
-           User capturedUser = userCaptor.getAllValues().get(1);
-           Map<String,String> userUpdatedInfo = userService.getUserInfo(user.getEmail());
-           int balance = userService.getUserBalance(user.getEmail());
-           assertEquals("ccastano42",userUpdatedInfo.get("nickname"));
-           assertEquals(1400,balance);
-           assertEquals("ccastano42",capturedUser.getNickName());
-           assertEquals(1400,capturedUser.getBalance());
-       }catch (InterruptedException e){
-           Thread.currentThread().interrupt();
-       }catch (UserException e){
-           fail(e.getMessage());
-       }
-   }
+            User capturedUser = userCaptor.getAllValues().get(1);
+            Map<String,String> userUpdatedInfo = userService.getUserInfo(user.getEmail());
+            int balance = userService.getUserBalance(user.getEmail());
+            assertEquals("ccastano42",userUpdatedInfo.get("nickname"));
+            assertEquals(1400,balance);
+            assertEquals("ccastano42",capturedUser.getNickName());
+            assertEquals(1400,capturedUser.getBalance());
+        }catch (InterruptedException e){
+            Thread.currentThread().interrupt();
+        }catch (UserException e){
+            fail(e.getMessage());
+        }
+    }
 
-   @Test
+    @Test
     void shouldConcurrentTransaction(){
-       User user = new User("casbsuw@mail.com", "milo45",2000,"image");
-       when(userRepository.findByEmail(user.getEmail())).thenReturn(user);
+        User user = new User("casbsuw@mail.com", "milo45",2000,"image");
+        when(userRepository.findByEmail(user.getEmail())).thenReturn(user);
 
 
-       Thread thread1 = new Thread(() -> {
-           try {
-               userService.transaction("casbsuw@mail.com", 1000);
-           } catch (UserException e){
-               assertEquals(UserException.NEGATIVE_BALANCE,e.getMessage());
-           }
-       });
+        Thread thread1 = new Thread(() -> {
+            try {
+                userService.transaction("casbsuw@mail.com", 1000);
+            } catch (UserException e){
+                assertEquals(UserException.NEGATIVE_BALANCE,e.getMessage());
+            }
+        });
 
-       Thread thread2 = new Thread(() -> {
-           try {
-               userService.transaction("casbsuw@mail.com", -2500);
-           } catch (UserException e){
-               assertEquals(UserException.NEGATIVE_BALANCE,e.getMessage());
-           }
-       });
+        Thread thread2 = new Thread(() -> {
+            try {
+                userService.transaction("casbsuw@mail.com", -2500);
+            } catch (UserException e){
+                assertEquals(UserException.NEGATIVE_BALANCE,e.getMessage());
+            }
+        });
 
-       thread1.start();
-       thread2.start();
+        thread1.start();
+        thread2.start();
 
-       try{
-           thread1.join();
-           thread2.join();
-           ArgumentCaptor<User> userCaptor = ArgumentCaptor.forClass(User.class);
-           verify(userRepository, atLeastOnce()).save(userCaptor.capture());
-           User capturedUser = userCaptor.getValue();
-           int balance = userService.getUserBalance(user.getEmail());
-           assertTrue(balance == 500 || balance == 3000);
-           assertTrue(capturedUser.getBalance() == 500 || capturedUser.getBalance() == 3000);
+        try{
+            thread1.join();
+            thread2.join();
+            ArgumentCaptor<User> userCaptor = ArgumentCaptor.forClass(User.class);
+            verify(userRepository, atLeastOnce()).save(userCaptor.capture());
+            User capturedUser = userCaptor.getValue();
+            int balance = userService.getUserBalance(user.getEmail());
+            assertTrue(balance == 500 || balance == 3000);
+            assertTrue(capturedUser.getBalance() == 500 || capturedUser.getBalance() == 3000);
 
-       }catch (InterruptedException e){
-           Thread.currentThread().interrupt();
-       }catch (UserException e){
-           fail(e.getMessage());
-       }
+        }catch (InterruptedException e){
+            Thread.currentThread().interrupt();
+        }catch (UserException e){
+            fail(e.getMessage());
+        }
+    }
 
+    @Test
+    void shouldNickNameTransaction(){
+        try{
+            User user = new User("casbsuw@mail.com", "milo45",500,"image");
+            when(userRepository.findByNickName(user.getNickName())).thenReturn(user);
+            when(userRepository.save(any(User.class))).thenReturn(user);
+            userService.transactionByNickname("milo45",50000);
+            userService.transactionByNickname("milo45",-10000);
 
-   }
+            assertEquals(40500,userService.getUsernickNameBalance("milo45"));
+
+        }catch (UserException e){
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    void shouldNotNullNickTransaction(){
+        try{
+            new User("casbsuw@mail.com", "milo45",500,"image");
+            userService.transactionByNickname("milo45",100);
+
+            fail("Not exception");
+        }catch (UserException e){
+            assertEquals(UserException.USER_NOT_FOUND,e.getMessage());
+        }
+    }
+
+    @Test
+    void shouldNotNegativeTransaction(){
+        try{
+            User user = new User("casbsuw@mail.com", "milo45",500,"image");
+            when(userRepository.findByNickName(user.getNickName())).thenReturn(user);
+            userService.transactionByNickname("milo45",-501);
+            fail("Not exception");
+        }catch (UserException e){
+            assertEquals(UserException.NEGATIVE_BALANCE,e.getMessage());
+        }
+    }
+
+    @Test
+    void shouldNotNullBalance(){
+        try{
+            new User("casbsuw@mail.com", "milo45",500,"image");
+            userService.getUsernickNameBalance("milo45");
+
+            fail("Not exception");
+        }catch (UserException e){
+            assertEquals(UserException.USER_NOT_FOUND,e.getMessage());
+        }
+    }
 
 }
